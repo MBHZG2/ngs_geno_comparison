03ef7142f091c1cf9fe44ff9dd05c2fa03f7bcbd
Mohamad Ballan <128239860+MBHZG2@users.noreply.github.com>
2023-12-12 15:03:22 +0100

    Update github.sh

--- github.sh
+++ github.sh
@@ -1,7 +1,1 @@
-echo "# ngs_geno_comparison" >> README.md
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/MBHZG2/ngs_geno_comparison.git
-  git push -u origin main
+
