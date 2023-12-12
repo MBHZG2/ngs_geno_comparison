cc7029952ceadcc6a68f18c97233306838ac53f4
Your Name <mat.lifepluse@gmail.com>
2023-12-12 08:25:43 -0500

    compare_geno_pip

---
+++ github.sh
@@ -0,0 +1,7 @@
+echo "# ngs_geno_comparison" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/MBHZG2/ngs_geno_comparison.git
+  git push -u origin main
